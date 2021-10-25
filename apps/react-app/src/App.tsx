bbe6053d62d741fd9d4ef751534303d4afdc78a4
Matheus Kindrazki <matheuskindrazki@gmail.com>
2021-10-25 14:16:26 -0300

    feat: atualizando pacote para rodar com pnpm

--- apps/react-app/src/App.tsx
+++ apps/react-app/src/App.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 
 // import { Container } from './styles';
 
-const App: React.FC = () => (
-  <div>Aplicação buildada com @psdlabs/react-scripts</div>
-);
+const App: React.FC = () => <div>Aplicação buildada ola mundo</div>;
 
 export default App;
